6cb378945d635020f3ff076547778b0e36c73726
Logan Franken <loganfranken@gmail.com>
2016-01-16 00:36:34 -0800

    Added basic player jumping

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        if (Input.GetButton("Jump"))
+        {
+            Rigidbody2D rigidBody = GetComponent<Rigidbody2D>();
+            rigidBody.AddForce(new Vector2(0, 1), ForceMode2D.Impulse);
+        }
+    }
+
+}
