abf5ec7aa0445b864be6c380b64ad9af837e535c
Logan Franken <loganfranken@gmail.com>
2016-01-16 01:29:02 -0800

    Added long jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,13 +2,48 @@
 
 public class PlayerController : MonoBehaviour
 {
+    public Transform PlatformDetector;
+    public float PlatformDetectionRadius = 0.2f;
+    public LayerMask PlatformMask;
+
+    public float BaseJumpForce = 5f;
+
+    public float AddedLongJumpForce = 0.5f;
+    public float MaxLongJumpForce = 3f;
+
+    private bool _isGrounded = false;
+    private bool _isJumping = false;
+
+    private float _remainingLongJumpForce = 0f;
+
     void FixedUpdate()
     {
-        if (Input.GetButton("Jump"))
+        Rigidbody2D rigidBody = GetComponent<Rigidbody2D>();
+
+        _isGrounded = Physics2D.OverlapCircle(PlatformDetector.position, PlatformDetectionRadius, PlatformMask);
+
+        // Basic jump
+        if(_isGrounded)
         {
-            Rigidbody2D rigidBody = GetComponent<Rigidbody2D>();
-            rigidBody.AddForce(new Vector2(0, 1), ForceMode2D.Impulse);
+            _isJumping = false;
+            _remainingLongJumpForce = 0f;
+
+            if (Input.GetButton("Jump"))
+            {
+                rigidBody.AddForce(new Vector2(0, BaseJumpForce), ForceMode2D.Impulse);
+                _isJumping = true;
+                _remainingLongJumpForce = MaxLongJumpForce;
+            }
+        }
+
+        // Long jump (if user is holding down "jump" during the ascent of a jump)
+        if(_isJumping && rigidBody.velocity.y > 0 && _remainingLongJumpForce > 0)
+        {
+            if(Input.GetButton("Jump"))
+            {
+                rigidBody.AddForce(new Vector2(0, AddedLongJumpForce), ForceMode2D.Impulse);
+                _remainingLongJumpForce -= AddedLongJumpForce;
+            }
         }
     }
-
 }
