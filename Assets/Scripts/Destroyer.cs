1a093b9f4c4f96d6964148dd5ed711be7ad84029
Andrew W Hung <druphoria@utexas.edu>
2016-01-17 02:32:16 -0800

    added destroyer for recycling obstacles

---
+++ Assets/Scripts/Destroyer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Destroyer : MonoBehaviour {
+    [SerializeField]
+    private List<StackPool> pools;
+    [SerializeField]
+    private LayerMask obstacleLayerMask;
+
+    private Dictionary<string, StackPool> poolDictionary;
+
+    void Awake()
+    {
+        poolDictionary = new Dictionary<string, StackPool>();
+        for(int x = 0; x < pools.Count; x++)
+        {
+            StackPool pool = pools[x];
+            poolDictionary.Add(pool.pooledObject.tag, pool);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (1 << other.gameObject.layer == obstacleLayerMask.value)
+        {
+            StackPool pool = poolDictionary[other.gameObject.tag];
+            other.gameObject.SetActive(false);
+            pool.Push(other.gameObject);
+        }
+    }
+}
