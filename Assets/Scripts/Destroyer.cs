60eb5d1d211ac2a18a03fa97dcf274806a11235d
Andrew W Hung <druphoria@utexas.edu>
2016-02-22 07:35:57 -0800

    separated out level serialization code and put it into a separate component to lessen the delay between the two screens

--- Assets/Scripts/Destroyer.cs
+++ Assets/Scripts/Destroyer.cs
@@ -27,30 +27,6 @@ public class Destroyer : MonoBehaviour {
             StackPool pool = poolDictionary[other.gameObject.tag];
             other.gameObject.SetActive(false);
             pool.Push(other.gameObject);
-            SendObjectOverNetwork(other.gameObject);
-        }
-    }
-
-    void SendObjectOverNetwork(GameObject obj)
-    {
-        if (obj.tag == "Platform")
-        {
-            PhotonNetwork.RaiseEvent((int) NetworkEventCode.SpawnPlatform, (Vector2) obj.transform.position, true, null);
-        }
-        else if (obj.tag == "Terrain")
-        {
-            TerrainRenderer renderer = obj.GetComponent<TerrainRenderer>();
-            if (renderer.ColliderConfigured)
-            {
-                BoxCollider2D bc = renderer.GetComponent<BoxCollider2D>();
-                //1 tile per unit, so we can use size directly
-                TerrainBlock block = new TerrainBlock(obj.transform.position, (int)bc.size.x, (int)bc.size.y);
-                PhotonNetwork.RaiseEvent((int)NetworkEventCode.SpawnTerrain, block, true, null);
-            }
-        }
-        else if (obj.tag == "Obstacle")
-        {
-            PhotonNetwork.RaiseEvent((int) NetworkEventCode.SpawnObstacle, (Vector2)obj.transform.position, true, null);
         }
     }
 }
---
+++ Assets/Scripts/Networking/LevelSender.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using SerializableObjects;
+using System.Collections;
+
+public class LevelSender : MonoBehaviour {
+
+    [SerializeField]
+    private LayerMask obstacleLayerMask;
+
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        if (1 << other.gameObject.layer == obstacleLayerMask.value)
+        {
+            SendObjectOverNetwork(other.gameObject);
+        }
+    }
+
+    void SendObjectOverNetwork (GameObject obj)
+    {
+        if (obj.tag == "Platform")
+        {
+            PhotonNetwork.RaiseEvent((int)NetworkEventCode.SpawnPlatform, (Vector2)obj.transform.position, true, null);
+        }
+        else if (obj.tag == "Terrain")
+        {
+            TerrainRenderer renderer = obj.GetComponent<TerrainRenderer>();
+            if (renderer.ColliderConfigured)
+            {
+                BoxCollider2D bc = renderer.GetComponent<BoxCollider2D>();
+                //1 tile per unit, so we can use size directly
+                TerrainBlock block = new TerrainBlock(obj.transform.position, (int)bc.size.x, (int)bc.size.y);
+                PhotonNetwork.RaiseEvent((int)NetworkEventCode.SpawnTerrain, block, true, null);
+            }
+        }
+        else if (obj.tag == "Obstacle")
+        {
+            PhotonNetwork.RaiseEvent((int)NetworkEventCode.SpawnObstacle, (Vector2)obj.transform.position, true, null);
+        }
+    }
+}
